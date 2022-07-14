9de0547b7085b93a5e86a66315554947eaec70ff
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-07-14 20:13:59 +0200

    Add button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
-    <div >
-      Hello World
-    </div>
+    <div><Button>Join</Button></div>
   );
 }
 
---
+++ src/components/Button/Button.component.jsx
@@ -0,0 +1,5 @@
+import "./Button.styles.scss";
+
+export function Button(props) {
+  return <button className="button">{props.children}</button>;
+}
