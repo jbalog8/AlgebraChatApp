6e508b5de63f38ec0b7622c9a9b2ba59b696becf
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-07-15 20:41:14 +0200

    Add avatar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Avatar } from "./components/Avatar";
 import { MessageText } from "./components/MessageText";
 
 function App() {
-  return <MessageText displayName="Joisp" time="20:13">
-    Ready for the trip? I'm so excited I couldn't sleep!
-  </MessageText>;
+  return <div>
+    <Avatar text="JB" backgroundColor="lime" />
+    <MessageText displayName="Josip" time="20:13">
+      Ready for the trip? I'm so excited I couldn't sleep!
+    </MessageText>
+  </div>
 }
 
 export default App;
