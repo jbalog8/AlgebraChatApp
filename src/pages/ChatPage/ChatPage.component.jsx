d532266b5c4e95ced5684ddf26ccad475d5647b7
jbalog8 <93664494+jbalog8@users.noreply.github.com>
2022-07-20 21:14:34 +0200

    Update styles

--- src/pages/ChatPage/ChatPage.component.jsx
+++ src/pages/ChatPage/ChatPage.component.jsx
@@ -5,7 +5,9 @@ import { MessageForm } from "../../components/MessageForm";
 
 export function ChatPage(props) {
   if (props.error !== null) {
-    return <div className="chat-page__error">Failed to connect to chat room.</div>;
+    return (
+      <div className="chat-page__error">Failed to connect to chat room.</div>
+    );
   }
 
   if (!props.joinedRoom) {
